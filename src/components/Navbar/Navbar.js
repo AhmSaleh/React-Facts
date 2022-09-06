3efdc1e03507f0e0a153cb1823ee96f0552854d7
ahmsaleh <ahmedsaleh166@gmail.com>
2022-09-06 03:19:37 +0200

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../../images/logo.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={logo} className="nav--icon" />
+      <h3 className="nav--logo_text">React Facts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
+}
